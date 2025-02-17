00121964ba74a06fa7027a848309ada736acc747
mercyOyiwodu <mercyabu98@gmail.com>
2025-02-17 03:21:07 +0100

    ok

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,9 +5,9 @@ const upload = require('../utils/multer')
 
 router.post('/user',upload.array('familyPictures', 5),createUser)
 router.patch('/user/:id',upload.array('familyPictures', 5),updateUser)
-router.get('/user',upload.array('familyPictures', 5),getAllUsers)
-router.get('/user/:id',upload.array('familyPictures', 5),getOneUser)
-router.delete('/user/:id',upload.array('familyPictures', 5),deleteUser)
+router.get('/user',getAllUsers)
+router.get('/user/:id',getOneUser)
+router.delete('/user/:id',deleteUser)
 
 
 module.exports=router;
